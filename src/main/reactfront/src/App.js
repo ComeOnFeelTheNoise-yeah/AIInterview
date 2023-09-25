e1d99895773ac60e3f4d6b2b117cc22b6184f847
Jiwoo Yang <111224337+YangjiwooGN@users.noreply.github.com>
2023-09-26 01:25:03 +0900

    feat)update

--- src/main/reactfront/src/App.js
+++ src/main/reactfront/src/App.js
@@ -5,11 +5,12 @@ import SignUp from "./views/Authentication/SignUp";
 import Authentication from "./views/Authentication";
 import MainLayout from "./views/layouts/MainLayout";
 import ChatWindow from "./components/ChatWindow";
+import ProjectMain from "./views/ProjectMain";
 
 const App = () => {
 
   return (
-    <ChatWindow/>
+    <ProjectMain/>
   )
 } 
 

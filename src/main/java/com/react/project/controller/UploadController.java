f6e5a7578d10afc127ffb66430f62ce09849e718
Jiwoo Yang <111224337+YangjiwooGN@users.noreply.github.com>
2023-10-04 02:47:43 +0900

    feat)fix...

--- src/main/java/com/react/project/controller/UploadController.java
+++ src/main/java/com/react/project/controller/UploadController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Collections;
 
@@ -19,11 +21,7 @@ public class UploadController {
     @Autowired
     private S3Service s3Service;
 
-    @PostMapping
-    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
-        String fileName = s3Service.uploadFile(file);
-        return ResponseEntity.ok().body("File uploaded: " + fileName);
-    }
+    private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
 
     @PostMapping("/uploadProfileImage")
     public ResponseEntity<?> uploadProfileImage(@RequestParam("image") MultipartFile file) {
@@ -32,9 +30,11 @@ public class UploadController {
             // 파일을 S3에 업로드하고 URL을 가져옵니다.
             imageUrl = s3Service.uploadFile(file);
         } catch (Exception e) {
+            logger.error("Error uploading image to S3", e);
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
+        logger.info("Image successfully uploaded to S3 with URL: {}", imageUrl);
         // S3 URL을 반환합니다.
         return new ResponseEntity<>(Collections.singletonMap("imageUrl", imageUrl), HttpStatus.OK);
     }

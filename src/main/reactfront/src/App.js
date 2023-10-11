b8cf20033c4a6091400351a5c659fc2ce092a46b
Jiwoo Yang <111224337+YangjiwooGN@users.noreply.github.com>
2023-10-12 02:48:40 +0900

    feat)update

--- src/main/reactfront/src/App.js
+++ src/main/reactfront/src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
 import React from 'react';
 import MainLayout from "./views/layouts/MainLayout";
-import NewsList from "./components/NewsList";
-import CompanyIntro from "./views/CompanyIntro";
-import Analysis from "./views/Analysis";
 import Payment from "./views/Payment";
 
 const App = () => {
 
   return (
-    <MainLayout/>
+    <Payment/>
   )
 } 
 
--- src/main/reactfront/src/views/Payment/index.js
+++ src/main/reactfront/src/views/Payment/index.js
@@ -1,24 +1,43 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import BootPay from "bootpay-js";
+import {Button} from "@mui/material";
 
 export default function Payment(){
 
-    const onPayment = () => {
-        console.log(BootPay);
+    const onPayment = (duration) => {
+        const productName = `프리미엄 ${duration}일 이용권`;
+
+        // 가격 동적 할당
+        let price;
+        switch (duration) {
+            case 30:
+                price = '100';
+                break;
+            case 90:
+                price = '200';
+                break;
+            case 180:
+                price = '300';
+                break;
+            default:
+                alert("선택한 이용 기간이 잘못되었습니다.");
+                return;
+        }
+
         BootPay.request({
-            price: '100',
+            price: price,
             application_id: "651fdab0d25985001abb83d6",
-            name: '프리미엄 30일 월정액',
+            name: productName,
             pg: 'inicis',
             method: 'card', //결제수단, 입력하지 않으면 결제수단 선택부터 화면이 시작합니다.
             show_agree_window: 0, // 부트페이 정보 동의 창 보이기 여부
-            order_id: 'premium30', //고유 주문번호로, 생성하신 값을 보내주셔야 합니다.
+            order_id: `premium${duration}`, //고유 주문번호로, 생성하신 값을 보내주셔야 합니다.
         }).error(function (data) {
-            //결제 진행시 에러가 발생하면 수행됩니다.
+            alert("결제가 취소되었습니다")
             console.log(data);
         }).cancel(function (data) {
-            //결제가 취소되면 수행됩니다.
+            alert("결제가 취소되었습니다")
             console.log(data);
         }).ready(function (data) {
             // 가상계좌 입금 계좌번호가 발급되면 호출되는 함수입니다.
@@ -37,13 +56,16 @@ export default function Payment(){
             // 결제창이 닫힐때 수행됩니다. (성공,실패,취소에 상관없이 모두 수행됨)
             console.log(data);
         }).done(function (data) {
-            //결제가 정상적으로 완료되면 수행됩니다
-            //비즈니스 로직을 수행하기 전에 결제 유효성 검증을 하시길 추천합니다.
+            alert("결제가 완료되었습니다")
             console.log(data);
         });
     };
 
     return (
-        <button onClick={onPayment} >결제하기</button>
+        <div>
+            <Button variant="contained" color="primary" onClick={() => onPayment(30)}>30일 결제</Button>
+            <Button variant="contained" color="primary" onClick={() => onPayment(90)}>90일 결제</Button>
+            <Button variant="contained" color="primary" onClick={() => onPayment(180)}>180일 결제</Button>
+        </div>
     );
 }
